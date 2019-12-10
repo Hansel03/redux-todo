38f950ebc5ffc8435924a3d144dbe26982e175f7
Hansel <hanselcapillo@hotmail.com>
2019-12-09 20:49:45 -0400

    Controlar los TODO-Items

--- src/app/todo/todo-item/todo-item.component.ts
+++ src/app/todo/todo-item/todo-item.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from "@angular/core";
+import { Todo } from "../model/todo.model";
+import { FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-todo-item',
-  templateUrl: './todo-item.component.html',
+  selector: "app-todo-item",
+  templateUrl: "./todo-item.component.html",
   styles: []
 })
 export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+  @ViewChild("txtInputFisico", { static: false }) txtInputFisico: ElementRef;
+  chkField: FormControl;
+  txtInput: FormControl;
+  editando: boolean;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.chkField = new FormControl(this.todo.completado);
+    this.txtInput = new FormControl(this.todo.texto, Validators.required);
   }
 
+  public editar() {
+    this.editando = true;
+    setTimeout(() => {
+      this.txtInputFisico.nativeElement.select();
+    });
+  }
+
+  public terminarEdicion() {
+    this.editando = false;
+  }
 }
